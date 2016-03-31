f74e5b4150db4ddc2cc0538374ecb4cebbd0d59d
Xiaoyu He <xiaoyuhe@Jonathans-MacBook-Air-2.local>
2016-03-31 07:07:15 -0700

    add event listener on change of currentImage

--- client/views/AppView.js
+++ client/views/AppView.js
@@ -3,6 +3,9 @@ var AppView = Backbone.View.extend({
   initialize: function() {
     this.currentImageView = new CurrentImageView({model: this.model.get('currentImage')});
     this.imageListView = new ImageListView({collection: this.model.get('imageList')});
+    this.model.on('change:currentImage', function(image) {
+      this.currentImageView.setImage(image.get('currentImage'));
+    }, this);
   },
 
   render: function() {
