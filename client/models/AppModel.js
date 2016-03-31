ae33b6bcdab0b174cb9fb6df695364d8315387d3
Xiaoyu He <xiaoyuhe@Jonathans-MacBook-Air-2.local>
2016-03-31 07:09:47 -0700

    add event listner and set currentImage

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -1,12 +1,10 @@
 var AppModel = Backbone.Model.extend({
 
   initialize: function(params){
-    // this.set('currentImage', new ImageModel());
-    // this.set('imageList', new ImageList());
-    // need to add events
-    // params.imageList.on('click', function(image) {
-    //   this.set();
-    // }, this);
-  }
+    this.set('currentImage', new ImageModel());
 
+    params.imageList.on('select', function(image) {
+      this.set('currentImage', image);
+    }, this);
+  }
 });
