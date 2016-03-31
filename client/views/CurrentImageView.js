518f4dc96bf22e59d4a645135a887b4e1fa4a378
Xiaoyu He <xiaoyuhe@Jonathans-MacBook-Air-2.local>
2016-03-31 08:44:16 -0700

    add dropdown rating menu

--- client/views/CurrentImageView.js
+++ client/views/CurrentImageView.js
@@ -1,6 +1,16 @@
 var CurrentImageView = Backbone.View.extend({
 
-  template: _.template('<img src=<%= url %> /><div><%= title %></div>'),
+  template: _.template('<img src=<%= url %> /><div><%= title %></div><div><%= rating %> out of 5</div>'),
+
+  dropdown: function(num){
+    var $menu = $('<select>');
+    var $option = $('<option>');
+    for (var i = 0; i <= num; i++) {
+      $menu.append($('<option>').text(i));
+    }
+    console.log($menu);
+    return $menu;
+  },
 
   initialize: function() {
 
@@ -12,7 +22,10 @@ var CurrentImageView = Backbone.View.extend({
   },
 
   render: function() {
-    return this.$el.html(this.template(this.model.attributes));
+    return this.$el.html([
+      this.template(this.model.attributes),
+      this.dropdown(5)
+      ]);
   }
   
 });
